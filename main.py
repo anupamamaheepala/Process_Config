df3560c88a7d73ddd97df3f09c89015f600e362c
anupamamaheepala <127319824+anupamamaheepala@users.noreply.github.com>
2025-03-18 10:02:12 +0530

    25_03_18_2

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from actionManipulation
-from utils.loggers import get_logger
+from actionManipulation import main as action_main
+from utils.logger import get_logger
 
-logger=get_logger("status_logger")
+# Initialize the logger
+logger = get_logger("status_logger")
 
 if __name__ == "__main__":
-    logger.info("Starting main.py")
-    actionManipulation.main()
-    logger.info("Finished main.py")
+    try:
+        logger.info("Starting main.py")
+
+        # Call the main function from actionManipulation
+        action_main()
+
+        logger.info("Finished main.py")
+
+    except Exception as e:
+        logger.error(f"An error occurred during the execution of main.py: {e}")
--- src/main.py
+++
@@ -1,4 +0,0 @@
-from process_config import fetch_and_update_process_config
-
-if __name__ == "__main__":
-    fetch_and_update_process_config()
