1a246a45003434316fbd96b8969895a75687173f
DulhanPerera <vicksura.20210165@iit.ac.lk>
2025-03-24 15:22:18 +0530

    new

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     # Define the account number, incident ID, and API URL
     account_num = "0000003746"
     incident_id = "3"
-    api_url = "http://220.247.224.226:9571/docs"  # Replace with the actual API URL
+    api_url = "http://220.247.224.226:9571/Request_Incident_External_information"  # Replace with the actual API URL
 
     # Process the incident
     process_incident(account_num, incident_id, api_url)
